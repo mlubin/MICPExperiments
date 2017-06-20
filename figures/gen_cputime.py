c064cf02e339cf97e6410d67ae191e7d6ef355a5
Miles Lubin <miles.lubin@gmail.com>
2017-06-20 14:29:45 -0400

    source to generate figures

---
+++ figures/gen_cputime.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+from perfprof import *
+
+
+names = []
+bonmin = []
+pajarito = []
+scip = []
+knitro = []
+cplex = []
+
+offset = 10.0
+
+fd = open('results.txt')
+for line in fd:
+    name, conetype, iter1, pajarito_t, iter2, bonmin_t, scip_t, knitro_t, cplex_t = line.split('&')
+    names.append(name.strip())
+    bonmin.append(float(bonmin_t.strip())+offset)
+    pajarito.append(float(pajarito_t.strip())+offset)
+    scip.append(float(scip_t.strip())+offset)
+    knitro.append(float(knitro_t.strip())+offset)
+    #pajarito_1.append(float(pajarito_1_t))
+
+print "%f" % np.min(bonmin)
+print "%f" % np.min(pajarito)
+
+print names
+print bonmin
+print pajarito
+
+from perfprof import *
+import matplotlib.pyplot as plt
+import numpy as np
+
+linespecs = ['k--', 'k-', 'k:', 'k-.']#, 'g-']
+labels = ['Bonmin OA', 'Pajarito', 'SCIP', "KNITRO"]#, 'pajarito_1']
+
+#plt.plot(bonmin, pajarito,'.')
+data = np.vstack((bonmin, pajarito, scip, knitro)).T#, pajarito_1)).T
+perfprof(data, linespecs=linespecs, legendnames=labels)
+plt.xlim((0.95,10.0))
+plt.legend(loc=4,prop={'size':24})
+#plt.ylim((0.0,1.0))
+plt.savefig(r'overall_cputime.png')
